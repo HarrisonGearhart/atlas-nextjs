ab2d0d3f7761ab7058dee735fb27f40b961b0dab
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-10-02 02:19:09 -0500

    fixed auth.ts/ Auth Sign-in & Out working task5 completed

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
+import { fetchUser } from "./lib/data";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -19,45 +20,21 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           type: "password",
         },
       },
+      //@ts-ignore
+      authorize: async (credentials: { email: string; password: string }) => {
+        const { email, password } = credentials;
+        const user = await fetchUser(email);
+        if (!user) return null; //@ts-ignore
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (passwordsMatch) return user;
+        return null;
+      },
     }),
   ],
-  callbacks: {},
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
 });
-
-Credentials({
-    credentials: {
-      email: {
-        label: "Email",
-      },
-      password: {
-        label: "Password",
-        type: "password",
-      },
-    },
-    //@ts-ignore
-    authorize: async (credentials: { email: string; password: string }) => {
-      const { email, password } = credentials;
-      const user = await fetchUser(email);
-      if (!user) return null; //@ts-ignore
-      const passwordsMatch = await bcrypt.compare(password, user.password);
-      if (passwordsMatch) return user;
-      return null;
-    },
-  }),
-
-  export const { handlers, signIn, signOut, auth } = NextAuth({
-    theme: {
-      brandColor: "#1ED2AF",
-      logo: "/logo.png",
-      buttonText: "#ffffff",
-    },
-    providers: [
-      Credentials({ ... }),
-    ],
-    callbacks: {
-      authorized: async ({ auth }) => {
-        // Logged in users are authenticated, otherwise redirect to login page
-        return !!auth;
-      },
-    },
-  });
